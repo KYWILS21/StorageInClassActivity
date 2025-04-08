b38377dbee5ac66d321c0542014bf551a7627bbc
KYWILS21 <tuk30460@temple.edu>
2025-04-08 12:10:29 -0400

    Fixed stuff

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.networkapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.view.menu.ShowableListMenu
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
@@ -14,10 +16,20 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
+import java.lang.StringBuilder
+import java.util.prefs.Preferences
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save comic info when downloaded
 // TODO (3: Automatically load previously saved comic when app starts)
+
+private const val AUTO_SAVE_KEY = "auto_save"
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +39,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private var autoSave = false
+    private lateinit var preferences: SharedPreferences
+    private lateinit var file: File
+    private val internalFilename = "my_file"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +55,33 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
+        preferences = getPreferences(MODE_PRIVATE)
+        file = File(filesDir, internalFilename)
+
+        autoSave = preferences.getBoolean(AUTO_SAVE_KEY, false)
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
+        loadComic()
+    }
 
+    private fun loadComic(){
+        if (file.exists()) {
+            try {
+                val br = BufferedReader(FileReader(file))
+                val text = StringBuilder()
+                var line: String?
+                while (br.readLine().also { line = it } != null) {
+                    text.append(line)
+                    text.append('\n')
+                }
+                br.close()
+                showComic(JSONObject(text.toString()))
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     // Fetches comic from web as JSONObject
@@ -51,7 +89,9 @@ class MainActivity : AppCompatActivity() {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
             JsonObjectRequest(url
-                , {showComic(it)}
+                , {
+                    saveComic(it)
+                    showComic(it)}
                 , {}
             )
         )
@@ -66,7 +106,9 @@ class MainActivity : AppCompatActivity() {
 
     // Implement this function
     private fun saveComic(comicObject: JSONObject) {
-
+        val outputStream = FileOutputStream(file)
+        outputStream.write(comicObject.toString().toByteArray())
+        outputStream.close()
     }
 
 
